302b5c62d0dde24b9218ba21564ac73587c7a9af
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2023-07-27 23:15:17 +0100

    Updated GitHub link to new one + made it look neater

--- src/Weather.js
+++ src/Weather.js
@@ -5,20 +5,17 @@ import "./App.css";
 export default function Weather() {
     return (
         <div className="html">
-    <h1>Todays Weather</h1>
-    <div className="search">
-      <form className="searchbutton" id="search-form">
-        <input
-          id="city-input"
-          type="text"
-          placeholder="Enter a place"
-          autocomplete="off"
-          autofocus="on" />
-        <input className="submitbutton" type="submit" value="Search" />
-      </form>
-      <h2>Date</h2>
-      <h3 id="city">Type in your location!</h3>
-      <div cclassName="container">
+            <h1>Todays Weather</h1>
+            <div className="search">
+                <form className="searchbutton" id="search-form">
+                    <input id="city-input" type="text"
+                        placeholder="Enter a place" autocomplete="off"
+                        autofocus="on" />
+                    <input className="submitbutton" type="submit" value="Search" />
+                </form>
+                <h2>Date</h2>
+                <h3 id="city">Type in your location!</h3>
+                <div className="container">
         <div className="box">
           <div className="column">
             <div className="col-6" id="first">
@@ -35,10 +32,10 @@ export default function Weather() {
         <img className="today-weather-icon" src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Clear" id="icon" />
         <div classNames="Week" id="forecast"></div>
       </div>
-    <h6>
-    <a href="https://github.com/BrookeClarke/Weather-App" target="_blank"> Open source code </a> by Brooke Clarke</h6>
+                <h6>
+                    <a href="https://github.com/BrookeClarke/weather-app-react" target="_blank"> Open source code </a> by Brooke Clarke</h6>
                 <p id="temperature-description"></p>
             </div>
-            </div>
+        </div>
     )
 }
